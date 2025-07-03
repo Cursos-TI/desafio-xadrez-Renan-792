700d9d2ea013d7f637d7b5d47ffc010b083ce732
Renan Silva <90579814+Renan-792@users.noreply.github.com>
2025-07-03 20:49:54 -0300

    Update xadrez.c
    Atualizando pra o nível iniciante

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,85 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+#include <stdio.h>
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+int main()
+{
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+	int torre = 0;
+	int bispo = 0;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+	int quantidade = 0;
+	char direcao;
 
+	printf("Digite a quantidade de casas da [Torre] (entre 1 e 8): ");
+	scanf("%d", &quantidade);
+
+	while(getchar() != '\n');
+
+	printf("\nEscolha a direcao (S > sobe / R > recuar / D > direita / E > Esquerda): ");
+	scanf("%c", &direcao);
+
+
+	if(quantidade < 1 || quantidade > 8) {
+		printf("Voce ultrapassou o limite permitido :/\n");
+
+	} else if (direcao == 'S' || direcao == 's') {
+		while(torre < quantidade) {
+			printf("Torre subindo\n" );
+			torre++;
+		}
+	} else if(direcao == 'R' || direcao == 'r') {
+		while(torre < quantidade) {
+			printf("Torre recuando\n");
+			torre++;
+		}
+	} else if(direcao == 'D' || direcao == 'd') {
+		while(torre < quantidade) {
+			printf ("Torre lado direito\n");
+			torre ++;
+		}
+	} else if(direcao == 'E' || direcao == 'e') {
+		while(torre >- quantidade) {
+			printf("Torre lado esquerdo\n");
+			torre--;
+		}
+	}  else {
+		printf("Zero nao sobe nem desce! \n");
+	}
+
+// BISPO
+
+	printf("\nMovimento do Bispo na diagonal:\n ");
+	printf("Digite a quantidade de casas da [BISPO] (entre 1 e 8): ");
+	scanf("%d", &quantidade);
+
+	if(quantidade < 1 || quantidade > 8 ) {
+		printf("\nNumero invalido!\n");
+	    } else {
+		    ("Bispo Movimentado-se para diagonal a direita\n");
+		  do{
+		      printf("Diagonal a direita\n");
+		      bispo ++;
+		  } while(bispo < quantidade);
+    }
+    
+    // RAINHA
+
+	printf("\nMovimento da Rainha:\n ");
+	printf("Digite a quantidade de casas da [RAINHA] (entre 1 e 8): ");
+	scanf("%d", &quantidade);
+	
+	if(quantidade < 1 || quantidade > 8){
+	    printf("Limite ultrapassado! ");
+	}else{
+	    for(int rainha= 1; rainha <= quantidade; rainha++ ){
+	        printf("Rainha move-se para Esquerda\n");
+	    }
+	}
+
+		return 0;
+}
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
